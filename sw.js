acb2a5d9705a8e0f87f67acd458371c298bc78b0
sandroregal <sandroregal@gmail.com>
2026-07-22 17:34:27 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE='copiloto-sp-v11';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{
+  const req=e.request;
+  if(req.method!=='GET') return;
+  e.respondWith(
+    fetch(req).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(c=>c.put(req,copy)).catch(()=>{});
+      return res;
+    }).catch(()=>caches.match(req).then(r=>r||caches.match('./index.html')))
+  );
+});
